8887fdeba3222743482c89e496508ca3bccd380f
Zach Marabeas <zmarabeas@gmail.com>
2025-06-15 13:15:49 -0400

    Update config

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   },
   root: path.resolve(import.meta.dirname, "client"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
+    outDir: path.resolve(import.meta.dirname, "dist"),
     emptyOutDir: true,
     rollupOptions: {
       input: 'client/src/main.tsx',
